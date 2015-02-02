b2323bda07ab34189960b1f25d58207298d2973c
hammel-j95 <hammelj95@gmail.com>
2015-02-01 21:51:05 -0500

    edits
    made some rather uncertain edits... Not sure if I'm keeping them.
    :sweat:

--- projectOne.js
+++ projectOne.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
   $("#goals").dialog({autoOpen:false});
     $("#goals-button").click(function() {
-      $("#goals")dialog("open");
+      $("#goals").dialog("open");
+  $("#goals").dialog({minHeight: 400});
+  $("#goals").dialog({minWidth: 600});
+  $("#goals").dialog({
+  buttons: [
+    {
+      text: "X",
+      click: function() {
+        $(this).dialog( "close" );
+      }
+    }
+  ]
+});
   });
 });
